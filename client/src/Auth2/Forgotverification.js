72b352f694c9737c8c3203e773cb58c448e1ad36
PABharath <150318512+PABharath@users.noreply.github.com>
2023-12-04 18:50:14 +0530

    forgotverify

--- client/src/Auth2/Forgotverification.js
+++ client/src/Auth2/Forgotverification.js
@@ -2,13 +2,45 @@ import React, { useState } from "react";
 import "./Forgotverification.css";
 // import { AiOutlineArrowLeft } from "react-icons/ai";
 import axios from 'axios';
-import { MdOutlineRemoveRedEye  } from "react-icons/md";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+
 
 function Forgotverification() {
   const [verificationCode, setVerificationCode] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [confirmNewPassword, setConfirmNewPassword] = useState('');
   const [message, setMessage] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [setpassword,SetConformpassword] = useState(false);
+
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+    const passwordInput = document.getElementById('password-input');
+  
+    if (passwordInput) {
+      if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+      } else {
+        passwordInput.type = 'password';
+      }
+    }
+
+  };
+  const togglePasswordVisibility2 = () => {
+    SetConformpassword((prev) => !prev);
+    const passwordInput = document.getElementById('password-input2');
+  
+    if (passwordInput) {
+      if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+      } else {
+        passwordInput.type = 'password';
+      }
+    }
+  };
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -67,23 +99,36 @@ function Forgotverification() {
             {/* <label>Confirm Password</label> */}
             <input
               type="password"
+              id="password-input"
               placeholder="new password"
               className="input1"
               value={newPassword}
               onChange={(e) => setNewPassword(e.target.value)}
-            /> <span className="icon1"
-            ><MdOutlineRemoveRedEye /></span>
+            />    <span className='icon1' onClick={togglePasswordVisibility}>
+            {showPassword ? (
+              <FontAwesomeIcon icon={faEyeSlash} />
+            ) : (
+              <FontAwesomeIcon icon={faEye} />
+            )}
+          </span>
+    
           </div>
            <div className="fgv-field fgv-input-field">
             {/* <label>Confirm New Password</label> */}
             <input
               type="password"
+              id="password-input2"
               placeholder="Confirm password"
               className="input1"
               value={confirmNewPassword}
               onChange={(e) => setConfirmNewPassword(e.target.value)}
-            /><span className="icon1"
-            ><MdOutlineRemoveRedEye /></span>
+            /><span className='icon1' onClick={togglePasswordVisibility2}>
+            {setpassword ? (
+              <FontAwesomeIcon icon={faEyeSlash} />
+            ) : (
+              <FontAwesomeIcon icon={faEye} />
+            )}
+          </span>
           </div> 
           <div className="fgv-field fgv-button-field">
             <button className="button" type="submit">Submit</button>
