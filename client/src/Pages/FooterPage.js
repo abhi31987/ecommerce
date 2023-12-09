33e2ac30a3f2254954db78709675d0caf6df9189
abhi31987 <135996982+abhi31987@users.noreply.github.com>
2023-12-09 15:29:22 +0530

    footer changes

--- client/src/Pages/FooterPage.js
+++ client/src/Pages/FooterPage.js
@@ -11,42 +11,56 @@ const Footer = () => {
     <div className="footer-container ">
       <div className="footer-wrapper">
         <div className="footer-section">
-        <Link to="/information" onClick={scrollToTop}><h3>INFORMATION</h3>
-          <ul><li>
-            <li>Terms & Conditions</li>
-            <li>Privacy Policy</li>
-            <li>Help & Support</li>
-            <li>Return Policy</li>
-            <li>Explore More</li>
+        <Link to="/information" onClick={scrollToTop}><h3 className='text12765'>INFORMATION</h3>
+       
+          <ul ><li>
+            <li className='list45'>Terms & Conditions</li>
+           
+           <li className='list45'>Privacy Policy</li> 
+           
+            <li className='list45'>Help & Support</li>
+           
+            <li className='list45'>Return Policy</li>
+           
+            <li className='list45'>Explore More</li>
             </li>
           </ul></Link>
         </div>
         <div className="footer-section">
-        <Link to="/Shop" onClick={scrollToTop}><h3>SHOP</h3>
+        <Link to="/Shop" onClick={scrollToTop}><h3 className='text12765'>SHOP</h3>
+       
           <ul><li>
-            <li>Wedding</li>
-            <li>Casual</li>
-            <li>Video Shopping</li>
+            <li className='list45'>Wedding</li>
+           
+            <li className='list45'>Casual</li>
+            
+            <li className='list45'>Video Shopping</li>
             </li>
           </ul></Link>
         </div>
         <div className="footer-section">
-        <Link to="/ContactUs" onClick={scrollToTop}><h3>CONTACT US</h3>
+        <Link to="/ContactUs" onClick={scrollToTop}><h3 className='text12765'>CONTACT US</h3>
+        
           <ul><li>
-            <li>PhoneNumber</li>
-            <li>Email</li>
-            <li>Location</li>
+            <li className='list45'>PhoneNumber</li>
+           
+            <li className='list45'>Email</li>
+          
+            <li className='list45'>Location</li>
             </li>
           </ul></Link>
         </div>
         <div className="footer-section">
-          <h3>Follow Us</h3>
+          <h3 className='text12765'>Follow Us</h3>
+         
           <ul className="social-media-icons">
             <li>
+            
               <a href="https://www.facebook.com">
                 <i className="fab fa-facebook-f"></i>
               </a>
             </li>
+            
             <li>
               <a href="https://www.twitter.com">
                 <i className="fab fa-twitter"></i>
@@ -61,6 +75,7 @@ const Footer = () => {
         </div>
       </div>
       <div className="footer-bottom"><hr/>
+      <br/>
         <p>&copy; 2023 Your Ecommerce Store. All rights reserved.</p>
       </div>
     </div>
